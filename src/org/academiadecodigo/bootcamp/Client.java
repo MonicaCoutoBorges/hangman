f4fe4fc94f25a88956088ede87f702873493d4b8
RodrigoDuarte <rodrigo.duarte.4618@gmail.com>
2017-12-27 14:56:11 -0100

    Server and client created

--- src/org/academiadecodigo/bootcamp/Client.java
+++ src/org/academiadecodigo/bootcamp/Client.java
@@ -1,4 +1,43 @@
 package org.academiadecodigo.bootcamp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    public static void main(String[] args) {
+        int port;
+        String hostName;
+        try {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Hostname: ");
+            hostName = scanner.nextLine();
+            System.out.print("Port number: ");
+            port = Integer.parseInt(scanner.nextLine());
+
+
+            Socket clientSocket = new Socket(hostName, port);
+            System.out.println("Connection established with " + hostName + " on port " + port + "\n");
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+            while (clientSocket.isBound()) {
+                System.out.print("Your message: ");
+                String message = scanner.nextLine();
+                out.println(message);
+                System.out.println(in.readLine());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/org/academiadecodigo/bootcamp/GameServer.java
+++ src/org/academiadecodigo/bootcamp/GameServer.java
@@ -1,6 +1,44 @@
 package org.academiadecodigo.bootcamp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class GameServer {
 
-    
+
+    public static void main(String[] args) {
+
+
+        int portNumber = 5000;
+
+
+        ServerSocket serverSocket = null;
+        System.out.println("Waiting for a connection on port " + portNumber + "...");
+        try {
+            serverSocket = new ServerSocket(portNumber);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connected");
+            while (clientSocket.isBound()) {
+
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String messageReceived = in.readLine();
+
+                System.out.println(messageReceived);
+                out.println("Your message " + "'" + messageReceived + "' was delivered.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
