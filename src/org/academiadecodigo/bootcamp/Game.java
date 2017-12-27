7a0b6a475a222257032c46a7d8b8873b187d5a43
RodrigoDuarte <rodrigo.duarte.4618@gmail.com>
2017-12-27 15:44:10 -0100

    properties and setters

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -2,8 +2,11 @@ package org.academiadecodigo.bootcamp;
 
 public class Game {
 
+    private String wordToGuess;
 
-    public void init(){
+
+
+    public void init() {
 
         //init gameserver
         //createplayer
@@ -14,4 +17,13 @@ public class Game {
 
     }
 
+    public void setPlayerName(Player player, String username) {
+        player.setName(username);
+    }
+
+
+
+    public void setWordToGuess(String wordToGuess) {
+        this.wordToGuess = wordToGuess;
+    }
 }
--- src/org/academiadecodigo/bootcamp/Player.java
+++ src/org/academiadecodigo/bootcamp/Player.java
@@ -1,4 +1,20 @@
 package org.academiadecodigo.bootcamp;
 
 public class Player {
+
+    private String name;
+
+
+
+
+    public void play(){
+
+
+
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
