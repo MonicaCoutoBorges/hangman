223650f379ba59e0c4d24225b33501453a439bc9
SoraiaVMV <soraiavmv@gmail.com>
2017-12-27 18:31:41 -0100

    some problems resolved

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -5,8 +5,8 @@ public class Game {
     private String wordToGuess;
     private String hiddenWord;
     private GameServer server;
-    private Player player1;
-    private Player player2;
+    private Player player1 = new Player();
+    private Player player2 = new Player();
     private int numberOfGuesses = 6;
     private boolean hasWon;
 
@@ -21,12 +21,16 @@ public class Game {
     public void start() {
 
         init();
+
         wordToGuess = player1.setWordToGuess();
         substituteWordCharacters();
 
         while (numberOfGuesses > 0 && !hasWon) {
             char character = player2.play();
-            compare(character);
+
+            if (!compareWords(character)){
+                numberOfGuesses--;
+            }
 
         }
 
@@ -37,6 +41,8 @@ public class Game {
 
     private void substituteWordCharacters() {
 
+        hiddenWord = "";
+
         for (int i = 0; i < wordToGuess.length(); i++) {
             hiddenWord += "_ ";
         }
@@ -45,21 +51,28 @@ public class Game {
     }
 
 
-    private void compare(char character) {
+    private boolean compareWords(char character) {
+
+        boolean attempt = false;
 
         for (int i = 0; i < wordToGuess.length(); i++) {
 
             if (wordToGuess.charAt(i) == character) {
-                hiddenWord.replace(hiddenWord.charAt(i * 2), character);
-                String visibleWord = hiddenWord.replace(" ", "");
 
-                if (visibleWord.equals(wordToGuess)) {
+                hiddenWord = hiddenWord.replace(hiddenWord.charAt(i * 2), character);
+                System.out.println(hiddenWord);
+
+                String finalWord = hiddenWord.replace(" ", "");
+
+                if (finalWord.equals(wordToGuess)) {
                     hasWon = true;
+                    System.out.println("Good job. You won the game.");
                 }
 
-            } else {
-                numberOfGuesses--;
+                attempt = true;
             }
         }
+
+        return attempt;
     }
 }
